93b722b5f2ba7b2af81fe90e3c343d9adbb3e69d
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-10-31 14:05:56 -0400

    fetch current questions and set to state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
 import QuestionContainer from "./containers/QuestionContainer";
 import "./App.css";
-var shuffle = require("shuffle-array");
 
 class App extends React.Component {
   state = {
-    questionsArr: [],
     isLoaded: true,
     playQuiz: false,
   };
 
-  componentDidMount() {
-    fetch("http://localhost:3000/questions.json")
-      .then((res) => res.json())
-      .then((data) =>
-        this.setState({
-          questionsArr: shuffle.pick(data, { picks: 10 }),
-        })
-      );
-  }
+
 
   playQuiz() {
     this.setState({ playQuiz: true });
@@ -30,7 +20,7 @@ class App extends React.Component {
         {!this.state.playQuiz ? (
           <button onClick={() => this.playQuiz()}>Test your Knowledge</button>
         ) : (
-          <QuestionContainer questions={this.state.questionsArr} />
+          <QuestionContainer />
         )}
       </div>
     );
--- src/containers/QuestionContainer.js
+++ src/containers/QuestionContainer.js
@@ -1,27 +1,33 @@
-import React, { Component } from 'react'
-import Question from "../presentational/Question.js";
+import React, { Component } from "react";
+// import Question from "../presentational/Question.js";
+var shuffle = require("shuffle-array");
 
-
-// componentDidMount(){
-  //  fetch("http://localhost:3000/questions.json)
-  //  .then(res => res.json())
-  //  .then(data => this.setState({
-    //      questionsArray: shuffle.pick((data),{'picks':10}),
-    //  }))
-    // }
 class QuestionContainer extends Component {
-
-
-
+  state = {
+    questionArr: [],
+    currentQuestion: {},
+    score: 0,
+    answered: false,
+  };
+  componentDidMount() {
+    fetch("http://localhost:3000/questions.json")
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          questionArr: shuffle.pick(data, { picks: 10 }),
+          currentQuestion: data[0],
+        })
+      );
+  }
 
   render() {
+    // console.log(this.state.currentQuestion);
     return (
       <div>
         <h1>Question Container</h1>
-
       </div>
-    )
+    );
   }
 }
 
-export default QuestionContainer
+export default QuestionContainer;
